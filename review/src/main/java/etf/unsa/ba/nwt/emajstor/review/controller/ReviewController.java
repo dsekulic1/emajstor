200ba381e2e3574c7be6927b2b1432aa552491a6
ekokic1 <ekokic1@etf.unsa.ba>
2022-03-30 21:54:34 +0200

    Implemented PATCH method using JSON Patch for Review Microservice

--- review/src/main/java/etf/unsa/ba/nwt/emajstor/review/controller/ReviewController.java
+++ review/src/main/java/etf/unsa/ba/nwt/emajstor/review/controller/ReviewController.java
@@ -1,10 +1,17 @@
 package etf.unsa.ba.nwt.emajstor.review.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
+import etf.unsa.ba.nwt.emajstor.review.exception.NotFoundException;
 import etf.unsa.ba.nwt.emajstor.review.model.Review;
 import etf.unsa.ba.nwt.emajstor.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -39,6 +46,29 @@ public class ReviewController {
     @PutMapping("/{id}")
     public ResponseEntity<Review> updateMessageById(@PathVariable UUID id, @RequestBody @Valid Review review ) {
         return ResponseEntity.ok(reviewService.updateReviewById(review, id));
+    }
+
+    // dodavavanje nove metode
+
+    private Review applyPatchToReview(
+            JsonPatch patch, Review targetReview) throws JsonPatchException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode patched = patch.apply(objectMapper.convertValue(targetReview, JsonNode.class));
+        return objectMapper.treeToValue(patched, Review.class);
+    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json")
+    public ResponseEntity<Review> updateReview(@PathVariable String id, @RequestBody JsonPatch patch) {
+        try {
+            Review review = reviewService.getReviewById(UUID.fromString(id));
+            Review reviewPatched = applyPatchToReview(patch, review);
+            reviewService.updateReviewById(reviewPatched, reviewPatched.getId());
+            return ResponseEntity.ok(reviewPatched);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
