e8d035d17c2e2bb0a18db26b2c79dcd2edb8959e
ekokic1 <ekokic1@etf.unsa.ba>
2022-03-30 22:20:12 +0200

    Implemented PATCH method using JSON Patch for Communication Microservice

--- communication/src/main/java/etf/unsa/ba/nwt/emajstor/communication/controller/MessageController.java
+++ communication/src/main/java/etf/unsa/ba/nwt/emajstor/communication/controller/MessageController.java
@@ -1,17 +1,25 @@
 package etf.unsa.ba.nwt.emajstor.communication.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
+import etf.unsa.ba.nwt.emajstor.communication.exception.NotFoundException;
 import etf.unsa.ba.nwt.emajstor.communication.model.Message;
 import etf.unsa.ba.nwt.emajstor.communication.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -57,6 +65,27 @@ public class MessageController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Message> deleteMessageById(@PathVariable UUID id) {
         return ResponseEntity.ok(messageService.deleteMessageById(id));
+    }
+
+    private Message applyPatchToMessage(
+            JsonPatch patch, Message targetMessage) throws JsonPatchException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode patched = patch.apply(objectMapper.convertValue(targetMessage, JsonNode.class));
+        return objectMapper.treeToValue(patched, Message.class);
+    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json")
+    public ResponseEntity<Message> updateReview(@PathVariable String id, @RequestBody JsonPatch patch) {
+        try {
+            Message message = messageService.getMessageById(UUID.fromString(id));
+            Message messagePatched = applyPatchToMessage(patch, message);
+            messageService.updateMessageById(messagePatched, messagePatched.getId());
+            return ResponseEntity.ok(messagePatched);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
