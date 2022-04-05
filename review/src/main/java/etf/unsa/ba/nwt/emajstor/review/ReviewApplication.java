94e003b6f7bdcb4242881becb736d233517558c4
Davor <dsekulic1@etf.unsa.ba>
2022-04-05 22:48:21 +0200

    config server on review service

--- review/src/main/java/etf/unsa/ba/nwt/emajstor/review/ReviewApplication.java
+++ review/src/main/java/etf/unsa/ba/nwt/emajstor/review/ReviewApplication.java
@@ -2,8 +2,10 @@ package etf.unsa.ba.nwt.emajstor.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReviewApplication {
 
 	public static void main(String[] args) {
