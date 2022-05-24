51e436e0e55a4eb95de8a6e94e2f6c7ccb5569f5
Tarik Japalak <tjapalak1@etf.unsa.ba>
2022-05-24 23:29:32 +0200

    dodavanje

--- ui/src/pages/Users/index.js
+++ ui/src/pages/Users/index.js
@@ -6,24 +6,63 @@ import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import Select from '@mui/material/Select';
 import Paper from '@material-ui/core/Paper'
 import SearchBar from 'material-ui-search-bar'
 import SettingsAccessibilityIcon from '@mui/icons-material/SettingsAccessibility'
+import TextField from '@mui/material/TextField'
+import Button from "@material-ui/core/Button";
+import MenuItem from '@mui/material/MenuItem';
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Modal from '@material-ui/core/Modal';
+import CssBaseline from '@mui/material/CssBaseline'
+import InputLabel from "@material-ui/core/InputLabel";
+import Container from '@mui/material/Container'
+import {Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { getAllUsers } from 'api/user/admin'
+import { getAuthConfig } from 'api/common'
+
 
 const useStyles = makeStyles({
     table: {
       minWidth: 650,
     },
   })
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   
 
 
 export default function Users() {
+  const [temprow, setTemprow] = useState()
   const [rows, setRows] = useState([])
   const [users, setUsers] = useState([])
   const [searched, setSearched] = useState('')
   const classes = useStyles()
+  const [permission, setPermission] = useState('')
+  // const [korisnik, setUser] = useState({firstName:"", lastName:"", email:"",number:"",username:"", password:"", city:""})
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (row)=>{
+    console.log(row)
+    setTemprow(row)
+    setPermission(row.role)
+    setOpen(true);}
+
+  const handleClose = () => setOpen(false);
+
 
   useEffect(() => {
     async function fetchData() {
@@ -49,6 +88,55 @@ export default function Users() {
     setSearched('')
     requestSearch(searched)
   }
+
+  let inputStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: '2px solid white',
+  }
+
+  const handlePermissionChange = (event) => {
+    setPermission(event.target.value)
+  }
+
+  const handleChange = (event) => {
+    // const value = event.target.value
+    // setUser({
+    //   ...korisnik,
+    //   [event.target.name]: value,
+    // })
+  }
+
+  const onFinish = async (values,id) => {
+    let config = getAuthConfig()
+    try {
+      const requestOptions = {
+        method: 'PATCH',
+        config,
+        body: JSON.stringify(values),
+      }
+      const response = await fetch(
+        'http://localhost:8081/api/users/'+id,
+        requestOptions
+      )
+      const data = await response.json()
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+ 
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+      const values = [{op:"/replace",path:"/role", value:permission}]
+
+    onFinish(values,temprow?.id)
+  }
+
   return (
     <>
     <Paper style={{ marginTop: '35px' }}>
@@ -77,7 +165,7 @@ export default function Users() {
                 <TableCell align='right'>{row?.contactInfo.firstName+" " +row?.contactInfo.lastName}</TableCell>
                 <TableCell align='right'>{row?.role}</TableCell>
                 <TableCell align='right'>{row?.contactInfo.email}</TableCell>
-                <TableCell align='right'><SettingsAccessibilityIcon></SettingsAccessibilityIcon>
+                <TableCell align='right'><SettingsAccessibilityIcon onClick={()=>handleOpen(row)}></SettingsAccessibilityIcon>
                 </TableCell>
               </TableRow>
             ))}
@@ -85,6 +173,166 @@ export default function Users() {
         </Table>
       </TableContainer>
     </Paper>
+    <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                disableEscapeKeyDown
+                disableEnforceFocus
+            >
+                <div style={inputStyle}>
+      <Container component='main' maxWidth='md'>
+        <CssBaseline />
+        <Box
+          sx={{
+            borderRadius: '5px',
+            backgroundColor: '#FFFAFA',
+            opacity: 0.9,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '25px',
+          }}
+        >
+          <Box
+            onSubmit={handleSubmit}
+            component='form'
+            noValidate
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2} marginBottom='10px' >
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  disabled={true}
+                  name='firstName'  
+                  required
+                  fullWidth
+                  id='firstName'
+                  label='First Name'
+                  value={temprow?.contactInfo.firstName}
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  disabled={true}
+                  required
+                  fullWidth
+                  id='lastName'
+                  label='Last Name'
+                  name='lastName'
+                  value={temprow?.contactInfo.lastName}
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled={true}
+                  required
+                  fullWidth
+                  id='email'
+                  label='Email Address'
+                  value={temprow?.contactInfo.email}
+                  onChange={handleChange}
+                  name='email'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled={true}
+                  required
+                  fullWidth
+                  id='number'
+                  label='Phone Number'
+                  name='number'
+                  type='tel'
+                  value={temprow?.contactInfo.number}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled={true}
+                  required
+                  fullWidth
+                  name='username'
+                  label='Username'
+                  type='username'
+                  id='username'
+                  onChange={handleChange}
+                  value={temprow?.username}
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <InputLabel id="demo-simple-select-helper-label">Role</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          name = "dropdown"
+          fullWidth
+          value={permission}
+          label="role"
+          onChange={handlePermissionChange}
+        >
+          <MenuItem value="">
+          </MenuItem>
+          <MenuItem value={"ROLE_ADMIN"}>Admin</MenuItem>
+          <MenuItem value={"ROLE_WORKER"}>Worker</MenuItem>
+          <MenuItem value={"ROLE_USER"}>User</MenuItem>
+        </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled={true}
+                  required
+                  fullWidth
+                  name='city'
+                  label='City'
+                  id='city' 
+                  onChange={handleChange}
+                  value={temprow?.city}
+                />
+              </Grid>
+              
+            </Grid>
+            <Button
+              type='submit'
+              variant='contained'
+              style={{
+                marginTop: '5%',
+                backgroundColor: '#3949AB',
+                marginLeft: '30%',
+                width: '40%',
+                color: '#ffff',
+                borderRadius: '10',
+              }}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Save Changes
+            </Button>
+            <Button
+            onClick={handleClose}
+            variant='contained'
+            style={{
+              marginTop: '2%',
+              backgroundColor: '#3949AB',
+              marginLeft: '30%',
+              width: '40%',
+              color: '#ffff',
+              borderRadius: '10',
+            }}
+            sx={{ mt: 3, mb: 2 }}>Close</Button>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+            </Modal>
+            
+        </div>
   </>
   )
 }
